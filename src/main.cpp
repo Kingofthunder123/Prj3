521b01db6d96cf97e52094e866a4759dd503bf2c
Kingofthunder123 <abelhamaker@gmail.com>
2024-04-17 13:53:44 +0200

    Testtest

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,12 @@
 Adafruit_VL6180X vl1 = Adafruit_VL6180X();
 Adafruit_VL6180X vl2 = Adafruit_VL6180X();
 
+Servo Sv;
+
 void setup() {
-  Serial.begin(9600);
-  
-  pinMode(2, OUTPUT);
+  Sv.attach(9, 500, 2500);
 
-  digitalWrite(2, LOW);
-
-  while (!Serial) {
-    delay(1);
-  }
-
-  Serial.println("Sensor 1 test!");
-  if (! vl1.begin()) {
-    Serial.println("Failed to find sensor 1");
-    while (1);
-  }
-  Serial.println("Sensor 1 found!");
-
-  vl1.setAddress(0x30);
-
-  digitalWrite(2, HIGH);
-  delay(22);
-
-  Serial.println("Sensor 2 test!");
-  if (! vl2.begin()) {
-    Serial.println("Failed to find sensor2");
-    while (1);
-  }
-  Serial.println("Sensor 2 found!");
-
-  Serial.println("done");
+  Sv.write(90);
 }
 
 void loop() {
