3319fd98240dd003e0e07d7d5b547c6fc0062c23
Kingofthunder123 <abelhamaker@gmail.com>
2024-04-19 10:11:42 +0200

    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,27 @@
 Adafruit_VL6180X vl1 = Adafruit_VL6180X();
 Adafruit_VL6180X vl2 = Adafruit_VL6180X();
 
+const int enableVl2 = 2;
+const int enableRGBs = 3;
+
 void setup() {
+  // Starts serial connection
   Serial.begin(9600);
   
-  pinMode(2, OUTPUT);
+  // Sets pinmodes
+  pinMode(enableVl2, OUTPUT);
+  pinMode(enableRGBs, OUTPUT);
 
-  digitalWrite(2, LOW);
+  // Disables ToF sensor vl2 and colour sensor
+  digitalWrite(enableVl2, LOW);
+  digitalWrite(enableRGBs, LOW);
 
+  // Waits for serial before continuing the program
   while (!Serial) {
     delay(1);
   }
 
+  // Begins communication with ToF sensor vl1
   Serial.println("Sensor 1 test!");
   if (! vl1.begin()) {
     Serial.println("Failed to find sensor 1");
@@ -25,11 +35,14 @@ void setup() {
   }
   Serial.println("Sensor 1 found!");
 
+  // Changes adress of ToF sensor vl1 from 0x29 to 0x30
   vl1.setAddress(0x30);
 
+  // Enables ToF sensor vl2 and wait for it to start
   digitalWrite(2, HIGH);
   delay(22);
 
+  // Begins communication with ToF sensor vl2
   Serial.println("Sensor 2 test!");
   if (! vl2.begin()) {
     Serial.println("Failed to find sensor2");
@@ -37,7 +50,10 @@ void setup() {
   }
   Serial.println("Sensor 2 found!");
 
-  Serial.println("done");
+  // Changes adress of ToF sensor vl2 from 0x29 to 0x31
+  vl2.setAddress(0x31);
+
+  Serial.println("Init done!");
 }
 
 void loop() {
