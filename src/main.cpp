fb23b9da058f582997b9457e998049a34b066ec3
Kingofthunder123 <abelhamaker@gmail.com>
2024-05-23 11:43:56 +0200

    Added base position and begin pick up function

--- src/main.cpp
+++ src/main.cpp
@@ -41,28 +41,43 @@ Servo svGripper;
 // Adafruit_VL6180X vl2 = Adafruit_VL6180X();
 // Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
+struct Pos{
+  int base;
+  int elbow;
+  int wrist;
+  int gripper;
+};
 
-//typedef enum {GRIPP_HOR, GRIPP_VER, RELEASE} ACTION;
 
 
+Pos startPos;
+
+typedef enum {HORIZONTAL, VERTICAL} ORIENTATION;
+
+void scanAndPickup(ORIENTATION dirDiabolo){};
 
 void setup() {
+
+  startPos.base     = 0;
+  startPos.elbow    = 30;
+  startPos.wrist    = 0;
+  startPos.gripper  = 15;
   // Starts serial connection
   Serial.begin(9600);
 
   // Attaches servo instances
-  // svBase.attach(servo1Pin, 500, 2500);
-  // svBase.write(0);
-  // delay(100);
-  svElbow.attach(servo2Pin, 500, 2500);
-  svElbow.write(30);
+  svBase.attach(servo1Pin, 500, 2500);
+  svBase.write(startPos.base);
   delay(100);
-  // svWrist.attach(servo3Pin, 500, 2500);
-  // svWrist.write(0);
-  // delay(100);
-  // svGripper.attach(servoGrPin, 500, 2500);
-  // svGripper.write(0 );
-  // delay(100);
+  svElbow.attach(servo2Pin, 500, 2500);
+  svElbow.write(startPos.elbow);
+  delay(100);
+  svWrist.attach(servo3Pin, 500, 2500);
+  svWrist.write(startPos.wrist);
+  delay(100);
+  svGripper.attach(servoGrPin, 500, 2500);
+  svGripper.write(startPos.gripper);
+  delay(100);
 
 
   // Sets Microsteppins as output
@@ -97,59 +112,16 @@ void setup() {
   // }
   // Serial.println("Vl1 found!");
 
-  // Changes adress of ToF sensor vl1 from 0x29 to 0x30
-  //vl1.setAddress(0x30);
-
-
-  // // !!!VL2!!!
-  // // Enables ToF sensor vl2 and wait for it to start
-  // digitalWrite(2, HIGH);
-  // delay(50);
-
-  // // Begins communication with ToF sensor vl2
-  // if (!vl2.begin()) {
-  //   Serial.println("Failed to find vl2");
-  //   while (1);
-  // }
-  // Serial.println("Vl2 found!");
-
-  // // Changes adress of ToF sensor vl2 from 0x29 to 0x31
-  // vl2.setAddress(0x31);
-
-
-  // // !!!COLOUR SENSOR!!!
-  // // Enable colour sensor
-  // digitalWrite(enableTcs, HIGH);
-  // delay(50);
-
-  // // Begins communication with colour sensor tcs
-  // if (!tcs.begin()) {
-  //   Serial.println("Failed to find tcs");
-  //   while (1);
-  // }
-  // Serial.println("Tcs found!");
-
-  // Serial.println("adresses:");
-  // Serial.print("vl1: ");
-  // Serial.println(vl1.getAddress(), HEX);
-  // Serial.print("vl2: ");
-  // // Serial.println(vl2.getAddress(), HEX);
-  // Serial.print("tcs: ");
-  // Serial.println(0x29, HEX);
-  // Serial.println("Init done!");
+  
 
   // set the maximum speed, acceleration factor,
 	// initial speed and the target position
 	turnStepper.setMaxSpeed(1000);
 	turnStepper.setAcceleration(1000);
 	turnStepper.setSpeed(1000);
-
-  
-  
-  
-  
 }
 
 void loop() {
+
 
 }
