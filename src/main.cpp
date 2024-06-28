0f37f0e8a02bfd86acf69ca87bfebb46e098a177
Kingofthunder123 <abelhamaker@gmail.com>
2024-06-28 12:23:29 +0200

    test sensor

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,43 @@
 #include <Wire.h>
 
 
-void setup() {
+
+VL6180X sensor;
+
+
+void setup()
+{
   Serial.begin(9600);
-  pinMode(39, INPUT);
+  Wire.begin();
+
+  sensor.init();
+  sensor.configureDefault();
+
+  // Reduce range max convergence time and ALS integration
+  // time to 30 ms and 50 ms, respectively, to allow 10 Hz
+  // operation (as suggested by table "Interleaved mode
+  // limits (10 Hz operation)" in the datasheet).
+  sensor.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
+  sensor.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
+
+  sensor.setTimeout(500);
+
+   // stop continuous mode if already active
+  sensor.stopContinuous();
+  // in case stopContinuous() triggered a single-shot
+  // measurement, wait for it to complete
+  delay(300);
+  // start interleaved continuous mode with period of 100 ms
+  sensor.startInterleavedContinuous(100);
+
 }
 
-void loop() {
-  Serial.println(digitalRead(39));
+void loop()
+{
+  
+  Serial.print("\tRange: ");
+  Serial.print(sensor.readRangeContinuousMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+  Serial.println();
 }
